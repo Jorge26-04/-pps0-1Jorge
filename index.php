7f1916db4a3afb5b957d6e11f9e1bd792e2eb91a
Jorge26-04 <132511279+Jorge26-04@users.noreply.github.com>
2025-03-24 18:46:56 +0000

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,6 @@
 <!DOCTYPE html>
 <html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Jorge</title>
-</head>
+
 <body>
     <h1>Hola mundo</h1>
     <h2>Prueba</h2>
